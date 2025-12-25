69cabcd0d789f0f7bc7ffafdd35a15e04d6cc4ae
masudxr <masudxr@example.com>
2025-12-25 22:27:39 +0600

    updated

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+     <h1>Hello Gamers Welcome to Our Journey !</h1>
+     <h2>Here Some Of my TOdo List</h2>
+
+    <>
+    <h1>Tasks List</h1>
+    <li>Login Panel</li>
+    <li>Management Drashboard</li>
+    <li>Students List with Total Info with Basic Salary</li>
+    <li>Teachers List with total Info with Basic Salary</li>
+    <li>Salary Panel for Students</li>
+    <li>Salary Panel for Teachers</li>
+    <li>Total Staff list</li>
+    </>
+    </>
+  );
+}
+
+export default App;
